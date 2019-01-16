bbce9575de81a91c7628099dde909bf96387c3a6
Aman Sharma <AmanSharma.pcmip@outlook.com>
2019-01-17 04:21:16 +1100

    Include guards added

--- inc/dictionary.h
+++ inc/dictionary.h
@@ -13,3 +13,9 @@ public:
     return false;
   }
 };
+#endif
+/*The include guards work like this: The first time dictionary.h is included,
+ the macro INC_DICTIONARY_H is not defined,  so the #ifndef test succeeds, and the code following the
+ #ifndef is processed by the compiler, which results in INC_DIRECTORY_H becoming defined.
+ Then, the next time the the header file is included,
+ the #ifndef test fails, and the code it controls is not compiled.*/
