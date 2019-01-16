e65c489a37b64ef9cdfd110cfe545b2f78c465c5
Aman Sharma <AmanSharma.pcmip@outlook.com>
2019-01-17 06:45:42 +1100

    Error Mechanism added

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,25 @@
 
 
 int main() {
-    std::cout << "scheck version 0.1" <<std::endl;
-    Dictionary d("data/mydict.data");
-    std::string word;
-
-    while(getline(std::cin,word))
+    try
     {
-        if(d.check(word))
-        {std::cout<<word<<" is okay"<<std::endl;}
-        else
-        {std::cout<<word<<" is Misspelled"<<std::endl;}
+        std::cout << "scheck version 0.1" <<std::endl;
+        Dictionary d("data/mydict.data");
+        std::string word;
+
+        while(getline(std::cin,word))
+        {
+            if(d.check(word))
+            {std::cout<<word<<" is okay\n"<<std::endl;}
+            else
+            {std::cout<<word<<" is Misspelled\n"<<std::endl;}
+        }
     }
+    catch (const ScheckError &e)
+    {
+        std::cerr <<" Error "<<e.what()<< "\n";
+    }
+
 
 
 }
