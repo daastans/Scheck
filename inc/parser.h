38c752f45fad85e909dfbc44d905af81524759af
Aman Sharma <AmanSharma.pcmip@outlook.com>
2019-01-19 05:34:06 +1100

    Added strstream

--- inc/parser.h
+++ inc/parser.h
@@ -1,12 +1,16 @@
 #ifndef INC_PARSER_H
 #define INC_PARSER_H
+#include<sstream>
 #include <iostream>
 #include<string>
 class Parser
 {
 private:
-    std::istream& mSubmission;          //You cannot copy istream so you have to take it as reference although you caould have used pointer
-
+    std::istream& mIn;          //You cannot copy istream so you have to take it as reference although you caould have used pointer
+    bool ReadLine();
+    std::string mLine;
+    unsigned int mLineNo;
+    std::istringstream mIs;
 public:
   Parser(std::istream &is);
   std::string NextWord();
