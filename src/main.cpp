7a154b7696fc1d6363c92b4f16a3b699f1ac2d77
Aman Sharma <AmanSharma.pcmip@outlook.com>
2019-01-18 21:35:48 +1100

    Added Parsing support

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include<string>
 #include "dictionary.h"
+#include "parser.h"
 
 
 int main() {
     try
     {
-        std::cout << "scheck version 0.1" <<std::endl;
+        std::cout << "scheck version 0.1\n" <<std::endl;
         Dictionary d("data/mydict.data");
         std::string word;
 
-        while(getline(std::cin,word))
+        std::ifstream sub("data/sub1.txt");
+
+        if(!sub.is_open()) throw ScheckError("cannot open data/sub1.txt");
+
+        Parser p(sub);
+
+        while((word=p.NextWord())!="")
         {
             if(d.check(word))
-            {std::cout<<word<<" is okay\n"<<std::endl;}
+            {std::cout<<word<<" is okay"<<std::endl;}
             else
-            {std::cout<<word<<" is Misspelled\n"<<std::endl;}
+            {std::cout<<word<<" is Misspelled"<<std::endl;}
         }
     }
     catch (const ScheckError &e)
