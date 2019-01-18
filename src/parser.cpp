6897a4f4077d8ac450a07a0c63ad477b91f6cd5c
Aman Sharma <AmanSharma.pcmip@outlook.com>
2019-01-19 05:31:06 +1100

    Implemented strstrem:ReadLine(),NextWord()

--- src/parser.cpp
+++ src/parser.cpp
@@ -4,12 +4,31 @@
 using std::istream;
 using std::string;
 
-Parser::Parser(istream& is):mSubmission(is) {}    // You cannot assign istream values so initializer list is used
+Parser::Parser(istream& is):mIn(is),mLineNo(0) {}    // You cannot assign istream values so initializer list is used
 
+unsigned int Parser::LineNo() const {return mLineNo;}
+string Parser::Context() const {return mLine;}
 string Parser::NextWord()
 {
     string word;
-    if(mSubmission>>word) return word;
-    else if(mSubmission.eof()) return "";
+    if(mIs>>word) return word;
+    else if(mIs.eof())
+    {
+        if(ReadLine()) return NextWord();
+        else return "";
+    }
+
     else throw ScheckError("Read Error");
+}
+bool Parser::ReadLine()
+{
+    if(getline(mIn,mLine))
+    {
+        mIs.clear();
+        mIs.str(mLine);
+        mLineNo++;
+        return true;
+    }
+    else if(mIn.eof()) return false;
+    else ScheckError("Couldnt read Input Stream");
 }
