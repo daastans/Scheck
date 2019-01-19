a589c4be4434c7f80817a2a88da76a98dc35c57f
Aman Sharma <AmanSharma.pcmip@outlook.com>
2019-01-19 22:17:02 +1100

    advance recognisation through states

--- src/parser.cpp
+++ src/parser.cpp
@@ -4,26 +4,63 @@
 using std::istream;
 using std::string;
 
-Parser::Parser(istream& is):mIn(is),mLineNo(0) {}    // You cannot assign istream values so initializer list is used
+Parser::Parser(istream& is):mIn(is),mLineNo(0),mPos(0),mState(stInSpace) {}    // You cannot assign istream values so initializer list is used
 
 unsigned int Parser::LineNo() const {return mLineNo;}
 string Parser::Context() const {return mLine;}
 string Parser::NextWord()
 {
     string word;
-    if(mIs>>word) return word;
-    else if(mIs.eof())
+
+    while(char c=NextChar())
     {
-        if(ReadLine()) return NextWord();
-        else return "";
+        switch(mState)
+        {
+        case stInSpace:
+            if(std::isalpha(c))
+            {
+                word+=c;
+                mState=stInWord;
+            }
+            else if(std::isdigit(c))
+            {
+                mState=stInNum;
+            }
+            break;
+        case stInWord:
+            if(std::isalpha(c)||c=='\'')
+                {
+                    word+=c;
+                }
+            else if(std::isdigit(c))
+                mState=stInNum;
+            else
+            {
+                mState=stInSpace;
+                return word;
+            }
+        case stInNum:
+            {
+                if(std::isalnum(c)||c=='\'')
+                    word+=c;
+                else
+                {
+                    mState=stInSpace;
+                    word="";
+                }
+        default: throw ScheckError("State Error");
+            }
+        }
     }
 
-    else throw ScheckError("Read Error");
+
 }
 bool Parser::ReadLine()
 {
     if(getline(mIn,mLine))
     {
+        mPos=0;
+        mLine+=" ";
         mIs.clear();
         mIs.str(mLine);
         mLineNo++;
@@ -31,4 +68,13 @@ bool Parser::ReadLine()
     }
     else if(mIn.eof()) return false;
     else ScheckError("Couldnt read Input Stream");
+}
+char Parser::NextChar()
+{
+    if(mPos>=mLine.size())
+    {
+        if(!ReadLine())
+            return 0;
+        return mLine[mPos++];
+    }
 }
